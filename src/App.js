2c3493edbbf1a7d8540c93844822f579d960fbfb
sookyeongyeom <sookyeong.dev@gmail.com>
2022-09-17 22:58:50 +0900

    :sparkles: import & export & data binding

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@
 
 import './App.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data';
 
 function App() {
+	let [shoes] = useState(data);
 	return (
 		<div className='App'>
 			<Navbar bg='light' variant='light'>
@@ -18,23 +21,25 @@ function App() {
 			<div className='main-bg'></div>
 			<div className='container'>
 				<div className='row'>
-					<div className='col-md-4'>
-						<img src='https://codingapple1.github.io/shop/shoes1.jpg' width='80%' />
-						<h4>상품명</h4>
-						<p>상품설명</p>
-					</div>
-					<div className='col-md-4'>
-						<img src='https://codingapple1.github.io/shop/shoes2.jpg' width='80%' />
-						<h4>상품명</h4>
-						<p>상품설명</p>
-					</div>
-					<div className='col-md-4'>
-						<img src='https://codingapple1.github.io/shop/shoes3.jpg' width='80%' />
-						<h4>상품명</h4>
-						<p>상품설명</p>
-					</div>
+					{data.map((v, i) => {
+						return <Shoe shoe={v} index={i} key={i} />;
+					})}
 				</div>
 			</div>
+		</div>
+	);
+}
+
+function Shoe(props) {
+	return (
+		<div className='col-md-4'>
+			<img src={`https://codingapple1.github.io/shop/shoes${props.index + 1}.jpg`} width='80%' />
+			<h4>{props.shoe.title}</h4>
+			<p>
+				{props.shoe.content}
+				<br />
+				{props.shoe.price}원
+			</p>
 		</div>
 	);
 }
