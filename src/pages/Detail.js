b2916faf9f04a4fa7357e40fee23efeeccc1b72e
sookyeongyeom <sookyeong.dev@gmail.com>
2022-09-18 00:36:16 +0900

    :sparkles: styled-components

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,6 +1,27 @@
 /* eslint-disable */
 
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+// styled-components
+// 장점1. 귀찮게 CSS파일까지 갈 필요없음
+// 장점2. 스타일이 다른 js파일로 오염되지않음 (.module.css도 지원하는 기능이긴함)
+// 장점3. 페이지 로딩시간 단축
+
+const Button = styled.button`
+	background: ${(props) => props.bg};
+	color: ${(props) => (props.bg === 'gray' ? 'white' : 'black')};
+	padding: 5px 10px;
+	border: none;
+	border-radius: 6px;
+	margin: 10px;
+`;
+
+const BlackBox = styled.div`
+	background-color: black;
+	color: white;
+	padding: 20px;
+`;
 
 function Detail(props) {
 	let { id } = useParams();
@@ -10,6 +31,10 @@ function Detail(props) {
 	return (
 		<>
 			<div className='container'>
+				{/* <BlackBox>
+					<Button bg='lightpink'>버튼</Button>
+					<Button bg='gray'>버튼</Button>
+				</BlackBox> */}
 				<div className='row'>
 					<div className='col-md-6'>
 						<img src={`https://codingapple1.github.io/shop/shoes${+id + 1}.jpg`} width='100%' />
