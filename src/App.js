2d5b36722480cf176ea3997398dd9998092fcc05
sookyeongyeom <sookyeong.dev@gmail.com>
2022-09-17 23:34:44 +0900

    :sparkles: default routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { useState } from 'react';
 import data from './data';
+import { Routes, Route, Link } from 'react-router-dom';
+import Detail from './pages/Detail';
 
 function App() {
 	let [shoes] = useState(data);
@@ -11,21 +13,36 @@ function App() {
 		<div className='App'>
 			<Navbar bg='light' variant='light'>
 				<Container>
-					<Navbar.Brand href='#home'>ShoeShop</Navbar.Brand>
+					<Navbar.Brand>ShoeShop</Navbar.Brand>
 					<Nav className='me-auto'>
-						<Nav.Link href='#home'>Home</Nav.Link>
-						<Nav.Link href='#cart'>Cart</Nav.Link>
+						<Link to='/' className='nav-link'>
+							Home
+						</Link>
+						<Link to='/detail' className='nav-link'>
+							Detail
+						</Link>
 					</Nav>
 				</Container>
 			</Navbar>
-			<div className='main-bg'></div>
-			<div className='container'>
-				<div className='row'>
-					{shoes.map((shoe, idx) => {
-						return <Shoe shoe={shoe} idx={idx} key={idx} />;
-					})}
-				</div>
-			</div>
+
+			<Routes>
+				<Route
+					path='/'
+					element={
+						<>
+							<div className='main-bg'></div>
+							<div className='container'>
+								<div className='row'>
+									{shoes.map((shoe, idx) => {
+										return <Shoe shoe={shoe} idx={idx} key={idx} />;
+									})}
+								</div>
+							</div>
+						</>
+					}
+				/>
+				<Route path='/detail' element={<Detail />} />
+			</Routes>
 		</div>
 	);
 }
