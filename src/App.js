3979620ffb3846fa0964999fb0f3c0021aa7efac
sookyeongyeom <sookyeong.dev@gmail.com>
2022-09-17 22:38:22 +0900

    :sparkles: bg + items image

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
+/* eslint-disable */
+
 import './App.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 
 function App() {
 	return (
 		<div className='App'>
-			<Navbar bg='dark' variant='dark'>
+			<Navbar bg='light' variant='light'>
 				<Container>
-					<Navbar.Brand href='#home'>Navbar</Navbar.Brand>
+					<Navbar.Brand href='#home'>ShoeShop</Navbar.Brand>
 					<Nav className='me-auto'>
 						<Nav.Link href='#home'>Home</Nav.Link>
-						<Nav.Link href='#features'>Features</Nav.Link>
-						<Nav.Link href='#pricing'>Pricing</Nav.Link>
+						<Nav.Link href='#features'>Cart</Nav.Link>
 					</Nav>
 				</Container>
 			</Navbar>
+			<div className='main-bg'></div>
+			<div className='container'>
+				<div className='row'>
+					<div className='col-md-4'>
+						<img src='https://codingapple1.github.io/shop/shoes1.jpg' width='80%' />
+						<h4>상품명</h4>
+						<p>상품설명</p>
+					</div>
+					<div className='col-md-4'>
+						<img src='https://codingapple1.github.io/shop/shoes2.jpg' width='80%' />
+						<h4>상품명</h4>
+						<p>상품설명</p>
+					</div>
+					<div className='col-md-4'>
+						<img src='https://codingapple1.github.io/shop/shoes3.jpg' width='80%' />
+						<h4>상품명</h4>
+						<p>상품설명</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
