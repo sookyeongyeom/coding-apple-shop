be19269a55ba015c1ec260067db03fa3a2c66237
sookyeongyeom <sookyeong.dev@gmail.com>
2022-09-18 23:23:02 +0900

    :sparkles: react/redux developer tools & lazy import

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@
 
 import './App.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import { createContext, useState } from 'react';
+import { createContext, lazy, Suspense, useState } from 'react';
 import data from './data';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
-import Detail from './pages/Detail';
-import Cart from './pages/Cart';
+// import Detail from './pages/Detail';
+// import Cart from './pages/Cart';
 import axios from 'axios';
 import styled from 'styled-components';
 import { useEffect } from 'react';
 import { useQuery } from 'react-query';
+
+const Detail = lazy(() => import('./pages/Detail.js'));
+const Cart = lazy(() => import('./pages/Cart.js'));
 
 const Button = styled.button`
 	background: ${(props) => props.bg};
@@ -91,104 +94,106 @@ function App() {
 				</Container>
 			</Navbar>
 
-			<Routes>
-				<Route
-					path='/'
-					element={
-						<>
-							<div className='main-bg'></div>
-							<div className='container'>
-								<div className='row'>
-									{shoes.map((shoe, idx) => {
-										return <Shoe shoe={shoe} idx={idx} key={idx} />;
-									})}
+			<Suspense fallback={<div>로딩중임...</div>}>
+				<Routes>
+					<Route
+						path='/'
+						element={
+							<>
+								<div className='main-bg'></div>
+								<div className='container'>
+									<div className='row'>
+										{shoes.map((shoe, idx) => {
+											return <Shoe shoe={shoe} idx={idx} key={idx} />;
+										})}
+									</div>
 								</div>
-							</div>
-							{/* 응용3. 버튼 누르면 로딩중 안내 띄우기 */}
-							{loading ? (
-								<h4 className='alert alert-warning' style={{ margin: '0 auto', width: '30%' }}>
-									로딩중이에영 ;ㅅ;
-								</h4>
-							) : null}
-							{/* 응용2. 버튼 3회 누르면 상품 더 없다고 알려주기 */}
-							{pageCount < 3 ? (
-								<Button
-									bg='lightpink'
-									onClick={() => {
-										// 로딩중 안내 ON
-										setLoading(true);
-										// ********************************************************
-										// axios = ajax요청 쉽게 해주는 라이브러리
-										axios
-											.get(`https://codingapple1.github.io/shop/data${pageCount + 1}.json`) //
-											.then((결과) => {
-												// axios는 array나 object를 알아서 json으로 변환해줌
-												const moreShoes = 결과.data;
-												// 신발 더보기
-												let copy = [...shoes, ...moreShoes];
-												setShoes(copy);
-												// 더보기 요청횟수 기록
-												// 응용1. 버튼 2회 누르면 7,8,9번 상품 가져오기
-												setPageCount(pageCount + 1);
-												// 로딩중 안내 OFF
-												setLoading(false);
-											})
-											.catch(() => {
-												console.log('실패함ㅠㅠ');
-												// 로딩중 안내 OFF
-												setLoading(false);
-											});
-										// ********************************************************
-										// fetch를 쓰는 경우 = 직접 json으로 변환해줘야함
-										// fetch('/url0')
-										// 	.then((결과) => 결과.json())
-										// 	.then((data) => {});
-										// ********************************************************
-										// 다수의 요청이 모두 완료된 후 어떤 동작을 실행하려면
-										// Promise.all([axios.get('/url1'), axios.get('/url2')]) //
-										// 	.then(() => {
-										// 		//실행 원하는 동작은 여기
-										// 	});
-									}}>
-									더보기
-								</Button>
-							) : (
-								<div className='alert alert-danger' style={{ margin: 0 }}>
-									상품 더 없어요!
-								</div>
-							)}
-						</>
-					}
-				/>
-				{/* URL Parameter */}
+								{/* 응용3. 버튼 누르면 로딩중 안내 띄우기 */}
+								{loading ? (
+									<h4 className='alert alert-warning' style={{ margin: '0 auto', width: '30%' }}>
+										로딩중이에영 ;ㅅ;
+									</h4>
+								) : null}
+								{/* 응용2. 버튼 3회 누르면 상품 더 없다고 알려주기 */}
+								{pageCount < 3 ? (
+									<Button
+										bg='lightpink'
+										onClick={() => {
+											// 로딩중 안내 ON
+											setLoading(true);
+											// ********************************************************
+											// axios = ajax요청 쉽게 해주는 라이브러리
+											axios
+												.get(`https://codingapple1.github.io/shop/data${pageCount + 1}.json`) //
+												.then((결과) => {
+													// axios는 array나 object를 알아서 json으로 변환해줌
+													const moreShoes = 결과.data;
+													// 신발 더보기
+													let copy = [...shoes, ...moreShoes];
+													setShoes(copy);
+													// 더보기 요청횟수 기록
+													// 응용1. 버튼 2회 누르면 7,8,9번 상품 가져오기
+													setPageCount(pageCount + 1);
+													// 로딩중 안내 OFF
+													setLoading(false);
+												})
+												.catch(() => {
+													console.log('실패함ㅠㅠ');
+													// 로딩중 안내 OFF
+													setLoading(false);
+												});
+											// ********************************************************
+											// fetch를 쓰는 경우 = 직접 json으로 변환해줘야함
+											// fetch('/url0')
+											// 	.then((결과) => 결과.json())
+											// 	.then((data) => {});
+											// ********************************************************
+											// 다수의 요청이 모두 완료된 후 어떤 동작을 실행하려면
+											// Promise.all([axios.get('/url1'), axios.get('/url2')]) //
+											// 	.then(() => {
+											// 		//실행 원하는 동작은 여기
+											// 	});
+										}}>
+										더보기
+									</Button>
+								) : (
+									<div className='alert alert-danger' style={{ margin: 0 }}>
+										상품 더 없어요!
+									</div>
+								)}
+							</>
+						}
+					/>
+					{/* URL Parameter */}
 
-				<Route
-					path='/detail/:id'
-					element={
-						// <Context이름.Provider>로 state공유를 원하는 컴포넌트 감싸기
-						<Context1.Provider value={{ 재고 }}>
-							<Detail shoes={shoes} />
-						</Context1.Provider>
-					}
-				/>
+					<Route
+						path='/detail/:id'
+						element={
+							// <Context이름.Provider>로 state공유를 원하는 컴포넌트 감싸기
+							<Context1.Provider value={{ 재고 }}>
+								<Detail shoes={shoes} />
+							</Context1.Provider>
+						}
+					/>
 
-				{/* Nested Routes */}
-				{/* 
+					{/* Nested Routes */}
+					{/* 
           장점1. 좀 더 직관적
           장점2. 상하위 element를 동시에 보여줄 수 있음 (부모라우트에 Outlet으로 자리 만들어줘야함)
         */}
-				<Route path='/about' element={<About />}>
-					<Route path='member' element={<div>멤버임</div>} />
-					<Route path='location' element={<div>위치정보임</div>} />
-				</Route>
-				<Route path='/cart' element={<Cart />} />
-				{/* 숙제 */}
-				<Route path='/event' element={<Event />}>
-					<Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>} />
-					<Route path='two' element={<div>생일기념 쿠폰받기</div>} />
-				</Route>
-				<Route path='*' element={<h4>404페이지</h4>} />
-			</Routes>
+					<Route path='/about' element={<About />}>
+						<Route path='member' element={<div>멤버임</div>} />
+						<Route path='location' element={<div>위치정보임</div>} />
+					</Route>
+					<Route path='/cart' element={<Cart />} />
+					{/* 숙제 */}
+					<Route path='/event' element={<Event />}>
+						<Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>} />
+						<Route path='two' element={<div>생일기념 쿠폰받기</div>} />
+					</Route>
+					<Route path='*' element={<h4>404페이지</h4>} />
+				</Routes>
+			</Suspense>
 		</div>
 	);
 }
